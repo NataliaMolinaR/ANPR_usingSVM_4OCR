194f150db9fd1235eefd7415f28c9b7d4c0bbb4a
Natalia <nsrmolina15@gmail.com>
2020-10-27 16:42:34 -0400

    Writting trainning examples in .csv file, 600 T.E.

--- Building_data.py
+++ Building_data.py
@@ -2,22 +2,20 @@ import csv
 import numpy as np
 import cv2
 import PPIF as pif
+import glob
 
 
-def reading_data():
+def reading_data(n):
 
-    n = 1
     file = './Base_datos/' + str(n) + '.jpg'
     src = cv2.imread(file)
 
-    cv2.imshow('letter_origin', src)
-    cv2.moveWindow('letter_origin', 20, 50)
+    src, _ = pif.resizing(src, src, 15)
 
-    src, _ = pif.resizing(src, src, 5)
-
-    cv2.imshow('letter', src)
-    cv2.moveWindow('letter', 100, 50)
+    cv2.imshow('Letra' + str(n), src)
+    cv2.moveWindow('Letra' + str(n), 100, 50)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     return src
 
@@ -29,19 +27,20 @@ def reducing_chanel(example):
 
 
 def run():
+    quantity_examplet = len(glob.glob('./Base_datos/*'))
+    print(quantity_examplet)
+    for n in range(601, quantity_examplet):
+        print(n)
+        example = reading_data(n)
+        data = reducing_chanel(example)
+        data = data.reshape(-1)
+        tag = int(input('Escribe la etiqueta del caracter: '))
+        training_example = np.append(data, tag)
+        # print(len(data),len(training_example))
+        with open('./data_base.csv',  'a', newline='') as file:
+            writer = csv.writer(file, lineterminator='\n')
+            writer.writerow(training_example)
 
-    example = reading_data()
-    data = reducing_chanel(example)
-
-    data = data.reshape(-1)
-
-    tag = int(input('Escribe la etiqueta del caracter: '))
-
-    training_example = np.append(data, tag)
-
-    with open('./data_base.csv',  'a', newline='') as file:
-        writer = csv.writer(file, lineterminator='\n')
-        writer.writerow(training_example)
 
 if __name__ == '__main__':
     run()
