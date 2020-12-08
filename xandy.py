eada5b107ba5cd05964d017aea2133db07594c9c
Natalia <nsrmolina15@gmail.com>
2020-12-08 09:54:07 -0400

    X and y matrix extracted

--- xandy.py
+++ xandy.py
@@ -1,15 +1,20 @@
 import numpy as np
+from sklearn import svm
 
 
 def run():
     with open("./data_base.csv") as file:
         n_cols = len(file.readline().split(";"))
         print(n_cols)
-    X = np.loadtxt("./data_base.csv", delimiter=";", usecols=np.arange(0, n_cols-1))
-    print(X.shape)
 
-    y = np.loadtxt("./data_base.csv", delimiter=";", usecols= n_cols)
+    X = np.loadtxt("./data_base.csv", delimiter=";", usecols=np.arange(0, n_cols - 1))
+    a = np.arange(0,n_cols)
+    print(a)
+    print(X.shape)
+    print(X[:, n_cols - 2])
+    y = np.loadtxt("./data_base.csv", delimiter=";", usecols= n_cols-1)
     print(y.shape)
+    print(y)
 
 
 
