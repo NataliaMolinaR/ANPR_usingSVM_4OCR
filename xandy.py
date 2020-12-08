05a97233febc2901d217febea638ccb93d2cc9a3
Natalia <nsrmolina15@gmail.com>
2020-12-08 12:28:25 -0400

    Preparping for trainning, adding sklearn comands

--- xandy.py
+++ xandy.py
@@ -7,16 +7,28 @@ def run():
         n_cols = len(file.readline().split(";"))
         print(n_cols)
 
-    X = np.loadtxt("./data_base.csv", delimiter=";", usecols=np.arange(0, n_cols - 1))
-    a = np.arange(0,n_cols)
-    print(a)
-    print(X.shape)
-    print(X[:, n_cols - 2])
-    y = np.loadtxt("./data_base.csv", delimiter=";", usecols= n_cols-1)
-    print(y.shape)
-    print(y)
+    X = np.loadtxt("./data_base.csv", delimiter=";", usecols= np.arange(0, n_cols - 1))
 
+    # a = np.arange(0, n_cols-1)
+    # print(a)
+    # print(X.shape)
+    # print(X[:, n_cols - 2])
 
+    Y = np.loadtxt("./data_base.csv", delimiter=";", usecols= n_cols-1)
+    # print(y.shape, n_cols - 1)
+    # print(y)
+
+    recon_char = svm.SVC(Kernel='linear', decision_function_shape='ovr')
+    recon_char.fit(X, Y)
+
+    w = recon_char.coef_[0]
+    b = recon_char.intercept_[0]
+
+    num_suportv = recon_char.n_support_
+
+    ind_suportv = recon_char.support_
+
+    suport_vec = recon_char.support_vectors_
 
 if __name__ == '__main__':
     run()
