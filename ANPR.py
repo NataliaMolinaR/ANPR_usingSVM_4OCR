3da51c3c5e6b77f0c6156fe8c9fae39764513cd4
Natalia <nsrmolina15@gmail.com>
2021-02-08 00:29:44 -0400

    Prueba 2  New cars

--- ANPR.py
+++ ANPR.py
@@ -38,7 +38,7 @@ def character2str(character, plate_len, index):
 
 def call_image():
 
-    file = './muestras/carro_5.jpg'
+    file = './fuente/matricula_179.jpg'
     src = cv2.imread(file)
     name_number = pf.calculting_name()
     return src, name_number
